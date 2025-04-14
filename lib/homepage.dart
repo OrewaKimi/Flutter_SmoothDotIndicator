22658e4b2b32be6c2060d326049144b8c39145c4
OrewaKimi <kimi93.com@gmail.com>
2025-04-14 22:04:33 +0700

    update code for path pages

---
+++ lib/homepage.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  final _controller= PageController();
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[200],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          // page view
+          SizedBox(
+            height: 500,
+            child: PageView(
+              controller: _controller,
+              children: const [
+                Page1(),
+                Page2(),
+                Page3(),
+              ],
+            ),)
+        ],
+      ),
+    );
+  }
+}
